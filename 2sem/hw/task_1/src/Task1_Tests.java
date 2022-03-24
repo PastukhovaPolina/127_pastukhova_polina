f6b8b12d76578224a91dd0716c8f0b82d7329c77
PastukhovaPolina <90262977+PastukhovaPolina@users.noreply.github.com>
2022-03-24 22:46:19 +0300

    тест getTail_PushBackDoubleLinkedList_checkGetTail, проверила

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -1,62 +1,69 @@
 import org.junit.*;
-public class Task1_Tests extends Assert{
+public class Task1_Tests extends Assert {
     @Test
-    public void isEmpty_DoubleLinkedList_checkIsEmpty(){
+    public void isEmpty_DoubleLinkedList_checkIsEmpty() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertTrue(list.isEmpty()); //проверка
     }
     @Test
-    public void isEmpty_DoubleLinkedList_checkPushBack(){
+    public void isEmpty_DoubleLinkedList_checkPushBack() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         list.pushBack(5);
         assertFalse(list.isEmpty());
     }
     @Test
-    public void getSize_DoubleLinkedList_checkGetSize(){
+    public void getSize_DoubleLinkedList_checkGetSize() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertTrue(0 == list.getSize());
     }
     @Test
-    public void pushFront_DoubleLinkedList_checkNumberGetSize(){
+    public void pushFront_DoubleLinkedList_checkNumberGetSize() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         list.pushFront(10);
         assertTrue(1 == list.getSize());
     }
     @Test
-    public void get_DoubleLinkedList_checkAnNumber(){
+    public void get_DoubleLinkedList_checkAnNumber() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         list.pushFront(1);
         try {
             list.get(2);
             fail();
-        } catch (IndexOutOfBoundsException x){ //ищем ошибку
+        } catch (IndexOutOfBoundsException e) { //ищем ошибку
             assertFalse(false);
         }
     }
     @Test
-    public  void getHead_DoubleLinkedList_checkGetHead(){
+    public void getHead_DoubleLinkedList_checkGetHead() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         list.pushFront(5);
         list.pushFront(2);
-        int y = ((Node<Integer>)list.getHead()).getData();
+        int y = ((Node<Integer>) list.getHead()).getData();
         assertEquals(5, y);
     }
     @Test
-    public void getHead_DoubleLinkedList_checkGetHead2(){
+    public void getHead_DoubleLinkedList_checkGetHead2() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         list.pushBack(6);
         list.pushBack(1);
-        int y = ((Node<Integer>)list.getHead()).getData();
+        int y = ((Node<Integer>) list.getHead()).getData();
         assertEquals(1, y);
     }
     @Test
-    public void getTail_DoubleLinkedList_checkGetTail(){
+    public void getTail_DoubleLinkedList_checkGetTail() {
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         list.pushFront(3);
         list.pushFront(2);
-        int t = ((Node<Integer>)list.getTail()).getData();
+        int t = ((Node<Integer>) list.getTail()).getData();
         assertEquals(2, t);
     }
-    
-
+    @Test
+    public void getTail_PushBackDoubleLinkedList_checkGetTail() {
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushBack(1);
+        list.pushBack(2);
+        list.pushBack(3);
+        int t = ((Node<Integer>) list.getTail()).getData();
+        assertEquals(1, t);
+    }
 }
