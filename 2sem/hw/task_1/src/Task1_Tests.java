67997cc104954500b3194478f1f0de378fb5ef74
PastukhovaPolina <90262977+PastukhovaPolina@users.noreply.github.com>
2022-03-25 01:06:53 +0300

    создала тест pushFront_PushFrontDoubleLinkedList_checkValues

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -146,6 +146,23 @@ public class Task1_Tests extends Assert {
         n = ((Node<Integer>)list1.get(5)).getData();
         assertEquals(6, n);
     }
+    @Test
+    public void pushFront_PushFrontDoubleLinkedList_checkValues(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        list.pushFront(1);
+        list.pushFront(2);
+        list.pushFront(3);
+        list.pushFront(4);
+        int n = ((Node<Integer>)list.get(0)).getData();
+        assertEquals(1, n);
+        n = ((Node<Integer>)list.get(1)).getData();
+        assertEquals(2, n);
+        n = ((Node<Integer>)list.get(2)).getData();
+        assertEquals(3, n);
+        n = ((Node<Integer>)list.get(3)).getData();
+        assertEquals(4, n);
+    }
+
 
 
 
