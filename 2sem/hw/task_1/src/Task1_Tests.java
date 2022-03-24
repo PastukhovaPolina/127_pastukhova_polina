167f935f72961413130ac606b8cf96f09aa70123
PastukhovaPolina <90262977+PastukhovaPolina@users.noreply.github.com>
2022-03-24 21:01:01 +0300

    создала тест getSize_DoubleLinkedList_checkGetSize, проверила

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -2,13 +2,18 @@ import org.junit.*;
 public class Task1_Tests extends Assert{
     @Test
     public void isEmpty_DoubleLinkedList_checkIsEmpty(){
-        DoubleLinkedList<Integer> list = new DoubleLinkedList<Integer>();
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
         assertTrue(list.isEmpty()); //проверка
     }
     @Test
     public void isEmpty_DoubleLinkedList_checkPushBack(){
         DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
-        list.pushBack(1);
+        list.pushBack(5);
         assertFalse(list.isEmpty());
+    }
+    @Test
+    public void getSize_DoubleLinkedList_checkGetSize(){
+        DoubleLinkedList<Integer> list = new DoubleLinkedList<>();
+        assertTrue(0 == list.getSize());
     }
 }
