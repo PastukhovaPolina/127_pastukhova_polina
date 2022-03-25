d6a3e0608bfad4c38b36818d58a4be4c83d1bb13
PastukhovaPolina <90262977+PastukhovaPolina@users.noreply.github.com>
2022-03-25 12:44:48 +0300

    тест resize_DynamicArray_checkSize

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -242,7 +242,12 @@ public class Task1_Tests extends Assert {
         DynamicArray<Integer> array = new DynamicArray<>();
         assertEquals(1024, array.getSize());
     }
-    
+    @Test
+    public void resize_DynamicArray_checkSize(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        array.resize(10);
+        assertEquals(10, array.getSize());
+    }
 
 
 
