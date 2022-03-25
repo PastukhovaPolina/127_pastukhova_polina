14a0fc709c370ba62a986efa93f30be972c5b459
PastukhovaPolina <90262977+PastukhovaPolina@users.noreply.github.com>
2022-03-25 13:58:40 +0300

    исправила

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -382,6 +382,17 @@ public class Task1_Tests extends Assert {
             assertEquals("Index out of bounds", some.getMessage());
         }
     }
+    @Test
+    public void remove_DynamicArray_checkException2(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.remove(5);
+            fail();
+        }
+        catch (Exception some) {
+            assertEquals("Index out of bounds", some.getMessage());
+        }
+    }
 
 
 
