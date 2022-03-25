64eb44789f28aac9a4ecf0588b462c6f809c99af
PastukhovaPolina <90262977+PastukhovaPolina@users.noreply.github.com>
2022-03-25 13:00:47 +0300

    тест get_Index2_exception

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -259,6 +259,17 @@ public class Task1_Tests extends Assert {
             assertEquals("Index out of bounds", some.getMessage());
         }
     }
+    @Test
+    public void get_Index2_exception(){
+        DynamicArray<Integer> array = new DynamicArray<>(5);
+        try {
+            array.get(5);
+            fail();
+        }
+        catch (IndexOutOfBoundsException some) {
+            assertEquals("Index out of b", some.getMessage());
+        }
+    }
 
 
 
