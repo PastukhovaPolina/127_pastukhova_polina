f355db9440774d13b17b8bfb10324a38537d31b6
PastukhovaPolina <90262977+PastukhovaPolina@users.noreply.github.com>
2022-03-25 13:37:37 +0300

    тест pushBack_DynamicArray_checkValues

--- 2sem/hw/task_1/src/Task1_Tests.java
+++ 2sem/hw/task_1/src/Task1_Tests.java
@@ -339,6 +339,21 @@ public class Task1_Tests extends Assert {
         assertEquals(4, array.get(3).intValue());
         assertEquals(100, array.get(4).intValue());
     }
+    @Test
+    public void pushBack_DynamicArray_checkValues(){
+        DynamicArray<Integer> array = new DynamicArray<>(3);
+        array.set(0, 1);
+        array.set(1, 2);
+        array.set(2, 3);
+
+        array.pushBack(4);
+        array.pushBack(5);
+        assertEquals(1, array.get(0).intValue());
+        assertEquals(2, array.get(1).intValue());
+        assertEquals(3, array.get(2).intValue());
+        assertEquals(4, array.get(3).intValue());
+        assertEquals(5, array.get(4).intValue());
+    }
 
 
 
